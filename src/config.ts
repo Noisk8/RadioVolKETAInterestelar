2420ef4d82e9858090f6bd0daf5dbf7abeccfa4e
Noisk8 <juansk8jaramillo@gmail.com>
2023-02-28 18:39:28 -0500

    metadata update

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Radio Volketa Interestelar";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "Orbitando frecuencias";
+export const TWITTER_HANDLE = "@noisk8";
+export const MY_NAME = "Radio Volketa Interestelar";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
