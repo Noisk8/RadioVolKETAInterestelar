8639aa78466c7a9f44684a7b7afa1ae0df93c98a
Noisk8 <juansk8jaramillo@gmail.com>
2025-08-15 18:23:38 -0500

    change titles

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Radio Volketa Interestelar";
+export const SITE_TITLE = "Radio ";
 export const SITE_DESCRIPTION =
   "Orbitando frecuencias";
 export const TWITTER_HANDLE = "@noisk8";
-export const MY_NAME = "Radio Volketa Interestelar";
+export const MY_NAME = "Radio";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
